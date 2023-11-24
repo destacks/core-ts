13c16b3d3c0dc06ad3c81549997ab5260f446ae5
Alexis Michaltsis <a.michaltsis@gmail.com>
2023-11-24 10:36:56 +0100

    Fix spacing in error message

--- packages/server-cli-only/cli.js
+++ packages/server-cli-only/cli.js
@@ -4,7 +4,7 @@ if (
   process.env.RUNNING_IN_CLI !== "true"
 ) {
   throw new Error(
-    "This module is not allowed in React Client Components." +
+    "This module is not allowed in React Client Components. " +
       "If you want to use it on the CLI, set RUNNING_IN_CLI=true on script run."
   );
 }
