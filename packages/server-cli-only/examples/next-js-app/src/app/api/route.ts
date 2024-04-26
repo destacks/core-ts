777521d5ed979b42bfe13033bc6f99502d3b8a6b
Marko Mitranic <marko.mitranic@gmail.com>
2024-04-26 12:32:06 +0200

    Add API routes

---
+++ packages/server-cli-only/examples/next-js-app/src/app/api/route.ts
@@ -0,0 +1,19 @@
+import { getSomeServerCliOnlyData } from "@/data";
+import { NextResponse, type NextRequest } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+/**
+ * @see http://localhost:3000/api
+ */
+export async function GET(request: NextRequest) {
+  try {
+    const data = await getSomeServerCliOnlyData(); // This is a server cli only function
+    return NextResponse.json({ status: 200, timestamp: Date.now(), data});
+  } catch (e: unknown) {
+    return NextResponse.json(
+      { e, status: 500, timestamp: Date.now() },
+      { status: 500 },
+    );
+  }
+}
