76db993c17d0a04ff096bafdf244efd23fea6172
Alexis Michaltsis <a.michaltsis@gmail.com>
2024-04-30 12:19:15 +0200

    Improve code formatting

--- packages/server-cli-only/error.default.js
+++ packages/server-cli-only/error.default.js
@@ -1,3 +1,5 @@
 throw new Error(
-  "[server-cli-only:error.default.js]: The module import fails for your safety. You are trying to run it in a runtime we don't know, so we hit the breaks. Imports of this module are only allowed in known server and edge runtimes."
+  "[server-cli-only:error.default.js]: Module import fails for your safety. " +
+    "You are trying to run it in a runtime we don't know, so we hit the breaks. " +
+    "Imports of this module are only allowed in known server and edge runtimes."
 );
