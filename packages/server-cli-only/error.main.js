c9a8f3220b6c1fa6cae0f9b8778db38980c82cc5
Alexis Michaltsis <a.michaltsis@gmail.com>
2024-04-30 12:18:59 +0200

    Fix info error: Node.js 12+ (not 11+) is correct

--- packages/server-cli-only/error.main.js
+++ packages/server-cli-only/error.main.js
@@ -1,3 +1,5 @@
 throw new Error(
-  "[server-cli-only:error.main.js]: Module import fails for your safety. Only Node.js version 11 and upwards can run this code."
+  "[server-cli-only:error.main.js]: Module import fails for your safety. " +
+    "Only Node.js 12+ can run this code. Needed feature of " +
+    '"conditional exports" was added to Node.js in v13.2.0 and v12.16.0.'
 );
